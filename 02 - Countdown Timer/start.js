0c14eb0ef261d8464651190b037d99ac37aabe55
b5710546232 <nattapat.su@ku.th>
2020-03-31 09:35:32 +0700

    finished day-02

--- 02 - Countdown Timer/start.js
+++ 02 - Countdown Timer/start.js
@@ -1,4 +1,33 @@
+"use strict";
 (() => {
   // เริ่มเขียนโค้ด
+  const SECOND = 1000;
+  const MINUTE = 60 * SECOND;
+  const HOUR = MINUTE * 60;
+  const DAY = HOUR * 24;
 
+  function setElementInnerText(id, text) {
+    const element = document.getElementById(id)
+    element.innerText = text
+  }
+
+  function countDonw() {
+    const now = new Date().getTime() // get unix time stamp.
+    const newYear = new Date(`December 31, 2020, 23:59:59`).getTime()
+    const unixTimeLeft = newYear - now;
+
+    setElementInnerText(`days`, Math.floor(unixTimeLeft / DAY));
+    setElementInnerText(`hours`, Math.floor((unixTimeLeft % DAY) / HOUR));
+    setElementInnerText(`minutes`, Math.floor((unixTimeLeft % HOUR) / MINUTE));
+    setElementInnerText(`seconds`, Math.floor((unixTimeLeft % MINUTE) / SECOND));
+
+  }
+
+  function run() {
+    setInterval(()=>{
+      countDonw()
+    },SECOND)
+  }
+
+  run();
 })();
